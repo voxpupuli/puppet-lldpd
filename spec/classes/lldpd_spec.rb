33e17b14df9f77f7578e2c608bcc10f4e0164aa3
Tim Meusel <tim@bastelfreak.de>
2017-11-18 16:06:46 +0100

    rubocop: autofix

--- spec/classes/lldpd_spec.rb
+++ spec/classes/lldpd_spec.rb
@@ -16,7 +16,7 @@ describe 'lldpd' do
                   else
                     false
                   end
-        facts.merge!({ systemd: systemd })
+        facts[:systemd] = systemd
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('lldpd') }
