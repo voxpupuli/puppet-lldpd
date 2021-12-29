f8d24e85770003410567ee2adba87bfb22875b21
Tim Meusel <tim@bastelfreak.de>
2021-12-29 12:12:10 +0100

    Configure external repo for tests

--- spec/acceptance/lldpd_spec.rb
+++ spec/acceptance/lldpd_spec.rb
@@ -22,4 +22,24 @@ describe 'lldpd class' do
       it { is_expected.to be_enabled }
     end
   end
+
+  context 'with managed repos' do
+    # Using puppet_apply as a helper
+    it 'works idempotently with no errors' do
+      pp = 'class { "lldpd": manage_repo => true}'
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    # do some basic checks
+    describe package('lldpd') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('lldpd') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+  end
 end
